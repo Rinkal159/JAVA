dadb5209f359cc73b7ae34ac2bcd47332f4d1959
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-07 22:28:50 +0530

    Pass by value

---
+++ PassByValue.java
@@ -0,0 +1,11 @@
+public class PassByValue {
+    public static int printMulty(int a) {
+        return a*=2;
+    }
+    public static void main(String[] args) {
+        int a = 2;
+
+        System.out.println(printMulty(a));
+        System.out.println(a);
+    }
+}
