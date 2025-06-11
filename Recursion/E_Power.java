5b59a3718d45d4cf710f3a490c414a50326269f1
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-11 17:09:30 +0530

    Power of n

---
+++ Recursion/E_Power.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class E_Power {
+    public static int printPower(int a, int n) {
+        if (n <= 0) {
+            return 1;
+        }
+        int helper = printPower(a, n-1);
+        System.out.println("helper : " + helper);
+        int pow = a * helper;
+        return pow;
+
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int a = 2;
+        int p = 1;
+
+        System.out.println(printPower(a, n));
+    }
+}
