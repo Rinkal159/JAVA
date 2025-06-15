787d5b3bf710a87c3b9e6bad8144d428a6ca55a8
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-15 22:03:46 +0530

    Take input and search for target

---
+++ TwoDArray/B_InputAndSearch.java
@@ -0,0 +1,34 @@
+package TwoDArray;
+
+import java.util.Scanner;
+
+public class B_InputAndSearch {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Count of row : ");
+        int row = x.nextInt();
+        System.out.print("Count of column : ");
+        int cols = x.nextInt();
+
+        int[][] arr = new int[row][cols];
+
+        System.out.println("Elements : ");
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = x.nextInt();
+            }
+        }
+
+        System.out.print("Target : ");
+        int a = x.nextInt();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (a == arr[i][j]) {
+                    System.out.println(a + " found at index (" + i + "," + j + ")");
+                }
+            }
+        }
+    }
+}
