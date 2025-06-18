b950c7d577e6a71a616573032a52c26451cc4113
Rinkal159 <rinkalsingapuri@gmail.com>
2025-06-18 09:58:01 +0530

    Clear range of bits.

---
+++ BitManipulation/G_ClearRangeOgBits.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+public class G_ClearRangeOgBits {
+    public static void ClearRange(int n, int i, int j) {
+        
+    }
+    public static void main(String[] args) {
+        int n = 2515;
+        int i=7;
+        int j= 2;
+    }
+}
