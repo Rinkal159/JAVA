a1158a6671f11bfc71ef1414f60715e95f40f25c
Rinkal159 <rinkalsingapuri@gmail.com>
2025-06-18 22:11:52 +0530

    Clear a range of bits in a number.

--- BitManipulation/G_ClearRangeOgBits.java
+++ BitManipulation/G_ClearRangeOgBits.java
@@ -2,11 +2,18 @@ package BitManipulation;
 
 public class G_ClearRangeOgBits {
     public static void ClearRange(int n, int i, int j) {
-        
+        int orOne = -1 << i+1;
+        int orTwo = ~(-1 << j);
+
+        int bitMask = orOne | orTwo;
+
+        System.out.println(bitMask & n);
     }
     public static void main(String[] args) {
-        int n = 2515;
-        int i=7;
-        int j= 2;
+        int n = 234;
+        int i=6;
+        int j= 4;
+
+        ClearRange(n, i, j);
     }
 }
