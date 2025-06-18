bd977eda8076ccd4024ac3eefe2a242f45a788c8
Rinkal159 <rinkalsingapuri@gmail.com>
2025-06-18 09:56:46 +0530

    Update a bit, make 0 if it is 1, make 1 if it is 0.

--- BitManipulation/D_Update.java
+++ BitManipulation/D_Update.java
@@ -2,19 +2,24 @@ package BitManipulation;
 
 public class D_Update {
     public static int updateBit(int a, int i) {
-        int bitMask = 1 << i;
 
-        if ((bitMask & a) == 0) {  //Get
-            return bitMask | a;  //Set
-        } else {
-            return (~(bitMask)) & a;  //Clear
+        //outdated approach
 
-        }
+        // int bitMask = 1 << i;
+
+        // if ((bitMask & a) == 0) { //Get
+        // return bitMask | a; //Set
+        // } else {
+        // return (~(bitMask)) & a; //Clear
+
+        // }
+
+        return ((1 << i) ^ a);  //XOR approach
     }
 
     public static void main(String[] args) {
         int a = 5;
-        int i = 1;
+        int i = 0;
 
         System.out.println("Number after updating a bit : " + (updateBit(a, i)));
 
