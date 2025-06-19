050fade8aa0313f7cf66f0b93fe83a988d167008
Rinkal159 <rinkalsingapuri@gmail.com>
2025-06-19 15:44:50 +0530

    Learn.

--- Learn.java
+++ Learn.java
@@ -1,20 +1,45 @@
-import java.util.*;
+class Student {
+    int[] marks;
+    String name;
+
+    public void printMarks() {
+        System.out.println(this.name);
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+
+        }
+    }
+
+    Student(String n) {
+        this.marks = new int[3];
+        this.name = n;
+    }
+
+    Student(Student s1) {
+        this.marks = s1.marks; //shallow copy
+
+        // { //deep copy
+        //     this.marks = new int[s1.marks.length];
+        //     for (int i = 0; i < marks.length; i++) {
+        //         this.marks[i] = s1.marks[i];
+        //     }
+        // }
+        this.name = s1.name;
+    }
+}
 
 public class Learn {
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
-        System.out.print("The number : ");
-        int a = x.nextInt();
-        int rem = 0;
-        int count = 0;
+        Student s1 = new Student("rinkal");
+        s1.marks[0] = 12;
+        s1.marks[1] = 20;
+        s1.marks[2] = 30;
 
-        while (a > 0) {
-            rem = a % 10;
-            a /= 10;
-            if (rem % 2 != 0) {
-                count++;
-            }
-        }
-        System.out.println("Number of odd digits : " + count);
+        Student s2 = new Student(s1);
+
+        s1.printMarks();
+        s1.marks[0] = 13; // will change because of pass by reference
+        s1.name = "priyanka"; // won't change because of pass by value
+        s2.printMarks();
     }
 }
