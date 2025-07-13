cde3a271b42890067bc0bd08e47594adf7bae016
Rinkal159 <rinkalsingapuri@gmail.com>
2025-07-13 09:17:51 +0530

    Removed scanner input comments.

--- BasicArrayFunctions/I_SubArraysSum.java
+++ BasicArrayFunctions/I_SubArraysSum.java
@@ -20,18 +20,6 @@ public class I_SubArraysSum {
     }
 
     public static void main(String[] args) {
-        // Scanner x = new Scanner(System.in);
-        // System.out.print("Size of the array : ");
-        // int size = x.nextInt();
-
-        // int[] arr = new int[size];
-
-        // System.out.println("Elements : ");
-
-        // for (int i = 0; i < size; i++) {
-        // arr[i] = x.nextInt();
-        // }
-
         int[] arr = { 2, 4, 6, 8, 10 };
 
         subArrays(arr);
