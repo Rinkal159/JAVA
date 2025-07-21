619833cec4b4410db898bcf0e593a4180e51e54e
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-21 20:23:33 +0530

    Print statement to debug the logic.

--- BackTracking/E_GridWay.java
+++ BackTracking/E_GridWay.java
@@ -4,21 +4,24 @@ package BackTracking;
 
 public class E_GridWay {
     public static int gridWays(int i, int j, int n, int m) {
-        if(i==n-1 && j==m-1) {
+        if (i == n - 1 && j == m - 1) {
             return 1;
-        } else if(i==n || j==m) {
+        } else if (i == n || j == m) {
             return 0;
         }
 
-        int wayDown = gridWays(i+1, j, n, m); 
-        int wayRight = gridWays(i, j+1, n, m);
+        int wayDown = gridWays(i + 1, j, n, m);
+        System.out.println("i : " + i);
+        int wayRight = gridWays(i, j + 1, n, m);
+        System.out.println("j : " + j);
 
         return wayDown + wayRight;
     }
-    public static void main(String[] args) {
-         int n = 4;
-         int m = 3;
 
-         System.out.println("Grid ways total : " + gridWays(0, 0, n, m));
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 3;
+
+        System.out.println("Grid ways total : " + gridWays(0, 0, n, m));
     }
 }
