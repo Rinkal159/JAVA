d7a3f39f74fba044c4bbdfc450be857c8afd5d26
rinkal159 <rinkalsingapuri@gmail.com>
2025-08-13 09:51:46 +0530

    Pure use of static method.

--- OOP/J_StaticMethod.java
+++ OOP/J_StaticMethod.java
@@ -1,27 +1,17 @@
 package OOP;
 
-import java.text.DecimalFormat;
-
-class Student {
-    public static String calcPercentage(double math, double chem, double phy) {
-        DecimalFormat df = new DecimalFormat("#.##");
-        String res = df.format((math + chem + phy) / 3);
-
-        return res;
+class Area {
+    public static void calcArea(int r) {
+        System.out.println("Area of given radius " + r + " is " + Math.PI * r * r);
     }
-
 }
 
 public class J_StaticMethod {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        System.out.println("Percentage : " + s1.calcPercentage(65, 94, 64));
-        Student s2 = new Student();
-        System.out.println("Percentage : " + s1.calcPercentage(100, 94, 44));
-        Student s3 = new Student();
-        System.out.println("Percentage : " + s1.calcPercentage(89, 10, 66));
-        Student s4 = new Student();
-        System.out.println("Percentage : " + s1.calcPercentage(76, 94, 64));
-
+        Area.calcArea(1);
+        Area.calcArea(2);
+        Area.calcArea(3);
+        Area.calcArea(4);
+        Area.calcArea(5);
     }
 }
