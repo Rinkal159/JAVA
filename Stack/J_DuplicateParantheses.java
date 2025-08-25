900bcbf32d2e07b2e134676e3e0603f7fd6c6fb0
rinkal159 <rinkalsingapuri@gmail.com>
2025-08-25 11:02:16 +0530

    Duplicate parantheses.

---
+++ Stack/J_DuplicateParantheses.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+// time complexity : O(n);
+
+public class J_DuplicateParantheses {
+    public static boolean duplicateParantheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int temp = Integer.MIN_VALUE;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+
+            if (currChar != '(' && currChar != ')') {
+                continue;
+            }
+
+            if (currChar == '(') {
+                stack.push(i);
+
+            } else if (currChar == ')') {
+                if (s.charAt(i - 1) == ')' && s.charAt(stack.peek() + 1) == '(' && temp == stack.peek() + 1) {
+                    return true;
+                }
+                if (i != s.length() - 1 && s.charAt(i + 1) == ')') {
+                    temp = stack.peek();
+                }
+                stack.pop();
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "((a+b) + ((e)))";
+
+        System.out.println("Duplicate parantheses : " + duplicateParantheses(s));
+    }
+}
