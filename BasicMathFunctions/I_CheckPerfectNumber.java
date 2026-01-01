3a79fddbc12eabf61ae72b7753630793435990aa
rinkal159 <rinkalsingapuri@gmail.com>
2026-01-01 09:12:54 +0530

    Changed the logic.

--- BasicMathFunctions/I_CheckPerfectNumber.java
+++ BasicMathFunctions/I_CheckPerfectNumber.java
@@ -1,39 +1,28 @@
 package BasicMathFunctions;
+
 import java.util.*;
 
 public class I_CheckPerfectNumber {
 
-    public static ArrayList<Integer> printDivisors(int a) {
+    public static boolean printDivisors(int a) {
         ArrayList<Integer> divisors = new ArrayList<>();
+        int sum = 0;
 
-        for (int i=1; i<a; i++) {
-            if(a%i==0) {
+        for (int i = 1; i < a; i++) {
+            if (a % i == 0) {
                 divisors.add(i);
+                sum += i;
             }
         }
-        return divisors;
+        return sum==a ? true : false;
     }
-
-    public static Boolean isPerfect(ArrayList<Integer> divisors, int a) {
-        int sumOfDivisors = 0;
-        for(int div : divisors) {
-            sumOfDivisors += div;
-        }
-
-        if(a == sumOfDivisors) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
 
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         System.out.print("The number : ");
         int a = x.nextInt();
 
-        System.out.println("Perfect Number : " + isPerfect(printDivisors(a), a));
+        System.out.println("Perfect Number : " + printDivisors(a));
 
         x.close();
     }
