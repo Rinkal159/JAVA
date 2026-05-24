d8ad5081ef9e48b7a3803245b2af9df42ba77c03
rinkal159 <rinkalsingapuri@gmail.com>
2026-05-24 11:47:15 +0530

    Introduces Treemap.

---
+++ Hashmap/C_Treemap.java
@@ -0,0 +1,14 @@
+//^ keys are sorted */
+//^ Time complexity changes: put, remove and get - O(logn ) */
+//* Red Black Trees is used in internal implementation */ 
+// Performance: TM < HM
+
+package Hashmap;
+
+import java.util.TreeMap;
+
+public class C_Treemap {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> tm = new TreeMap<>();
+    }
+}
