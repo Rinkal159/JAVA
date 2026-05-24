01657fc39d4768e0d59c9743e33ead85da1c3a0d
rinkal159 <rinkalsingapuri@gmail.com>
2026-05-24 11:47:06 +0530

    Introduces LinkedHashMap.

---
+++ Hashmap/B_LinkedHashmap.java
@@ -0,0 +1,14 @@
+//^ insertion ordered, meaning first inserted keys will be appeared first
+// Performance: LHM < HM
+
+package Hashmap;
+
+import java.util.LinkedHashMap;
+
+public class B_LinkedHashmap {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
+        // all the operations are same as HashMap
+
+    }
+}
