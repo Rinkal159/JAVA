8328ae7aab745d3e5cc5b2e939201aedc13a89f8
rinkal159 <rinkalsingapuri@gmail.com>
2026-05-24 11:48:39 +0530

    Introduces LinkedHashSet.

---
+++ Hashmap/G_LinkedHashSet.java
@@ -0,0 +1,14 @@
+//^ insertion ordered, meaning first inserted keys will be appeared first
+// Performance: LHS < HS
+
+package Hashmap;
+
+import java.util.LinkedHashSet;
+
+public class G_LinkedHashSet {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhm = new LinkedHashSet<>();
+        // all the operations are same as HashSet
+
+    }
+}
