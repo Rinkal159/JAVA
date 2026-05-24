3baec2a0adef6b348392efc5c995a0ccfa84c49c
rinkal159 <rinkalsingapuri@gmail.com>
2026-05-24 11:48:47 +0530

    Introduces TreeSet.

---
+++ Hashmap/H_TreeSet.java
@@ -0,0 +1,16 @@
+//^ keys are SORTED */
+//^ Time complexity changes: put, remove and get - O(logn) */
+//^ NULL values are NOT allowed 
+        // because it sorts the code, so it need comparison, and NULL values    can't be compared with other values 
+//^ Performance: TS < HS
+
+package Hashmap;
+
+import java.util.TreeSet;
+
+public class H_TreeSet {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        // all the operations are same as HashSet
+    }
+}
